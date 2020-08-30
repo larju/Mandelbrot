03ba49d2175ebcd43484dde3391898368f7cbb87
larju <larsjuv@gmail.com>
2020-08-30 03:28:42 +0200

    Implemented Mandelbrot set calculations & rendering them.

--- src/main/java/com/larju/gui/Mandelbrot.java
+++ src/main/java/com/larju/gui/Mandelbrot.java
@@ -16,6 +16,8 @@ public class Mandelbrot extends JPanel {
     private float stepSizeX;
     private float stepSizeY;
 
+    private final int MAX_ITERATIONS = 1500;
+
     public Mandelbrot() {
         super();
 
@@ -28,6 +30,39 @@ public class Mandelbrot extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        /*
+         * Below I'm following pseudocode from Wikipedia article linked
+         * in /README.md (under the section "Reading List", https://en.wikipedia.org/wiki/Mandelbrot_set#Computer_drawings).
+         *
+         * I do make my own modifications to this code.
+         */
+        // For each pixel on panel
+        for (int y = 0; y < Main.HEIGHT; y++) {
+            for (int x = 0; x < Main.WIDTH; x++) {
+                // Scaling coordinates to our range
+                double scaledX = scaleX(x);
+                double scaledY = scaleY(y);
+
+                int iterations = 0;                     // Number of current iterations
+                double iterationX = 0, iterationY = 0;  // Variables used for continuous iterations below
+
+                // Iterative process of iterationX & iterationY
+                while (Math.pow(iterationX, 2) + Math.pow(iterationY, 2) <= 4*4 && iterations < MAX_ITERATIONS) {
+                    double temporaryX = iterationX*iterationX - iterationY*iterationY + scaledX;
+                    iterationY = 2*iterationX*iterationY + scaledY;
+                    iterationX = temporaryX;
+
+                    iterations++;
+                }
+
+                /* Selecting color & drawing pixel */
+                g.setColor(new Color(iterations % 255));
+                g.fillRect(x, y, 1, 1);
+            }
+        }
+
+        System.out.println("Rendering done.");
     }
 
     /**
